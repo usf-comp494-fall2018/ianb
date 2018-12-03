dbcde0afc7829c009685a9dd5e1851cd3904fab2
Ian Bond <ianbond@stfrancis.edu>
2018-12-03 17:23:26 -0600

    I started a XAML project to call an api containing the current conditions and three day forcast.

---
+++ BondFinal/BondFinal/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using BondFinal.Models;
+//using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BondFinal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<NorthwindDisplayData> _dispay;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _dispay = new ObservableCollection<NorthwindDisplayData>();
+            results.ItemsSource = _dispay;
+        }
+
+        private void CurrentCondtionsButton_Click(object sender, RoutedEventArgs e)
+        {
+            _dispay.Clear();
+        }
+
+        private void ThreeDayForcastButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                _dispay.Clear();
+                var test = client.GetStringAsync("https://finalprojectcomp494.azurewebsites.net/api/weather/");
+                _dispay.Add(new NorthwindDisplayData()
+                {
+                    Info = test.ToString()
+                });
+            }
+        }
+    }
+}
---
+++ BondFinal/BondFinal/Models/ThreeDay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BondFinal.Models
+{
+    class ThreeDay
+    {
+        public int ThreeDayID { get; set; }
+        public int Period { get; set; }
+        public string Icon { get; set; }
+        public string Day { get; set; }
+        public string ThreeDayDesciption {get; set;}
+        public DateTime ThreeDayLastChecked { get; set; }
+    }
+}
